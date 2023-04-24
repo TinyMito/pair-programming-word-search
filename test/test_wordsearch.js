593539725df78944adca7d93927bfef944c2041a
TinyMito <k.ip@me.com>
2023-04-23 21:22:14 -0400

    First commit

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,86 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'UEUVEEPUU')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'UEUVAAPUU')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ASYH')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WEFMHFBDZASDAASDASD')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WEFMHFBD')
+
+    assert.isFalse(result);
+  });
 });
+
+// Assigned pair programming cohort wasn't available
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,59 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalTranspose = transpose(letters); // Call transpose function
+    const verticalJoin = verticalTranspose.map(ls => ls.join(''))
+
+    //console.log(horizontalJoin.length, verticalJoin.length) // Debug Only
+
+    // Check all array horizontally
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        //console.log('Horizontal: ', word, word.length) // Debug Only
+
+        if (word.length === verticalJoin.length) {
+            // Check if the word length match before searching
+            if (l.includes(word)){
+                // If found return true
+                return true;
+            } 
+        }
     }
+    // Check all array vertically from the transposed array
+    for (l of verticalJoin) {
+        //console.log('Vertical: ', word, word.length) // Debug Only
+
+        if (word.length === horizontalJoin.length) {
+            // Check if the word length match before searching
+            if (l.includes(word)){
+                // If found return true
+                return true;
+            }  
+        }
+    }
+
+    // Will always be false until found
+    return false;
+}
+
+// From previous matrix_transposition.js "There Is No Spoon" Pair Programming exercise
+const transpose = function(matrix) {
+    /* Get array parameters vertical and horizontal
+     * Transpose function will swap the array into vertical order
+     */
+    const vertical = matrix.length;
+    const horizontal = matrix[0].length;
+    const verticalArray = [];
+
+    for (let h = 0; h < horizontal; h++) {
+        verticalArray[h] = [];
+        for (let v = 0; v < vertical; v++) {
+            // Swap value into new array
+            verticalArray[h][v] = matrix[v][h];
+        }
+    }
+    // Once swapped, return the new array for vertical
+    return verticalArray;
 }
 
 module.exports = wordSearch
+
+// Assigned pair programming cohort wasn't available
